3047c78208145e13e1ca26e8b17be281bde45590
Paul Caballero <jeneralpaul0111@gmail.com>
2023-02-15 22:25:15 +0800

    Update GUI

--- javaScript/controls.js
+++ javaScript/controls.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
-    $('#basicPay').keyup(function() {
-        $(this).attr('size', $(this).val().length)
-    });
-});
+
+//$(document).ready(function() {
+//    $('#basicPay').keyup(function() {
+//        $(this).attr('size', $(this).val().length)
+//    });
+//});
